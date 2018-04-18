3375255c795c60acba253681f599776cf5675dcc
ae94 <ae94@icloud.com>
2018-04-18 16:59:03 +0200

    added local modules init (commented)

--- src/api-init.ts
+++ src/api-init.ts
@@ -1,6 +1,6 @@
 import { get } from 'https';
 import * as unzip from 'unzip';
-import { createWriteStream, readFileSync, writeFileSync } from 'fs';
+import { createWriteStream, readFileSync, writeFileSync, existsSync, readdirSync } from 'fs';
 import { join } from 'path';
 import * as request from 'request';
 import { exec } from 'child_process';
@@ -17,9 +17,11 @@ export default function initApi(name: string, gitUrl: string, awsProfile: string
             .on('close', () => {
                 reaplceFieldsInPackage(name, gitUrl, awsProfile)
                 npmInit().then(() => {
+                    // return tsc().then(() => {
                     return gitInit(gitUrl).then(() => {
                         resolve();
                     })
+                    // })
                 }).catch(err => {
                     reject(err);
                 })
@@ -34,7 +36,7 @@ export function reaplceFieldsInPackage(name: string, gitUrl: string, awsProfile:
         "type": "git",
         "url": gitUrl
     };
-    writeFileSync(join(process.cwd(), 'package.json'), JSON.stringify(packageJson))
+    writeFileSync(join(process.cwd(), 'package.json'), JSON.stringify(packageJson, null, 4))
 }
 export function npmInit() {
     return new Promise((resolve, reject) => {
@@ -48,6 +50,31 @@ export function npmInit() {
                 resolve()
             }
         })
+    })
+}
+export function tsc() {
+    return new Promise((resolve, reject) => {
+        if (existsSync(join(process.cwd(), 'local_modules'))) {
+            let dirs = readdirSync(join(process.cwd(), 'local_modules'))
+            Promise.all(dirs.map(dir => {
+                return new Promise((resolve, reject) => {
+                    exec('cd ' + join(process.cwd(), 'local_modules', dir) + ' && tsc', (err, stdout, stderr) => {
+                        console.log(stdout);
+                        console.error(stderr);
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve()
+                        }
+                    })
+                })
+            })).then(() => {
+                resolve()
+            }).catch(err => {
+                resolve(); // not mandatory
+            })
+
+        }
     })
 }
 export function gitInit(gitUrl: string) {
